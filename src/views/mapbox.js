1036fff8c5b0f21ecc974898ce7631b649abdc89
jiegiser <cjieouba@outlook.com>
2021-06-07 16:00:21 +0800

    Feat: 增加 mapbox js 文件

---
+++ src/views/mapbox.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiamllZ2lzZXJnZyIsImEiOiJjanExcmJjMTYxMGlxM3hueG9lZjQ4eng5In0.F4Ia4OCMj8HZV8scGQvSfQ";
+
+export default mapboxgl;
